98ea95f07e8400952f500ab8e449f21b2925bcee
Mantas Traigys <mantastraigys@Mantass-MacBook-Pro.local>
2024-02-29 09:44:32 +0200

    refactor: small package-provider change

--- src/package-provider/package-provider.module.ts
+++ src/package-provider/package-provider.module.ts
@@ -1,12 +1,25 @@
 import { Module } from '@nestjs/common';
-import { PackageProviderLocalDb } from './package-provider-local.db';
 import { DbPort } from './db.port';
+import { PackageName } from '../transaction/enums/package-name.enum';
+import { PackageSize } from '../transaction/enums/package-size.enum';
+import { PackageProvider } from '../transaction/types/package-provider.type';
+import { LocalDb } from './local.db';
 
 @Module({
   providers: [
     {
       provide: DbPort,
-      useClass: PackageProviderLocalDb,
+      useFactory: () => {
+        const packageProviders: PackageProvider[] = [
+          { name: PackageName.LaPoste, packageSize: PackageSize.Small, price: 1.5 },
+          { name: PackageName.LaPoste, packageSize: PackageSize.Medium, price: 4.9 },
+          { name: PackageName.LaPoste, packageSize: PackageSize.Large, price: 6.9 },
+          { name: PackageName.MondialRelay, packageSize: PackageSize.Small, price: 2.0 },
+          { name: PackageName.MondialRelay, packageSize: PackageSize.Medium, price: 3.0 },
+          { name: PackageName.MondialRelay, packageSize: PackageSize.Large, price: 4.0 },
+        ];
+        return new LocalDb(packageProviders);
+      },
     },
   ],
   exports: [DbPort],
